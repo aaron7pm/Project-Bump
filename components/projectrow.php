b9796d59b194ad4dd7f71363e974fe8e87d7840f
Aaron Dancer <me@aarondancer.com>
2015-05-04 01:41:00 -0500

    Improved mobile responsiveness of project rows

--- components/projectrow.php
+++ components/projectrow.php
@@ -7,7 +7,7 @@
 	</td>
 	<td>
 		<a href=<? echo $_SERVER['REQUEST_URI'] . "project.php?id=" . $ProjectID ?>><h4><? echo $title ?></h4></a>
-		<p><? echo $description ?></p>
+		<p class="hidden-xs"><? echo $description ?></p>
 	</td> 
 	<td class="text-right">
 		by<h4><? echo $user ?></h4>
