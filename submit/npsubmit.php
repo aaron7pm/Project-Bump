7e3379fa11da5f30e4a1cc5f104fcb435bc58604
Aaron Dancer <me@aarondancer.com>
2015-05-05 06:23:09 -0500

    Insignificant or empty image url will now set to null

--- submit/npsubmit.php
+++ submit/npsubmit.php
@@ -4,8 +4,10 @@
 	$website = $_POST["inputWebsite"];
 	$description = $_POST["inputDescription"];
 	$image = $_POST["inputImage"];
+	$image = "\"" . $image . "\"";
+	if (strlen($image)<5) $image = "null";
 	$UserID = $_COOKIE['UserID'];
-	$qstring = "CALL createProject($UserID, \"$name\", \"$description\", \"$image\", \"$website\")";
+	$qstring = "CALL createProject($UserID, \"$name\", \"$description\", $image, \"$website\")";
 	$userquery = $mysqli->query($qstring);
 	if ($userquery === TRUE) {
 		echo "<script>window.location = './index.php';</script>";
