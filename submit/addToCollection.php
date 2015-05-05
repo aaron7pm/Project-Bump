8ca30c163a1eb51f64b3ad6fa9eee0f1954bd008
Aaron Dancer <me@aarondancer.com>
2015-05-05 06:14:17 -0500

    Duplicate prevention

--- submit/addToCollection.php
+++ submit/addToCollection.php
@@ -3,6 +3,8 @@
 	$cid = $_POST["cid"];
 	$uid = $_POST["uid"];
 	$pid = $_POST["pid"];
-	$mysqli->query("CALL addToCollection(" . $cid . ", " . $uid . ", " . $pid . ")");
+	$exists = $mysqli->query("SELECT COUNT(1) AS ex FROM Collection 
+		WHERE CollectionID = " . $cid . " AND ProjectID = ". $pid)->fetch_assoc()["ex"];
+	if ($exists = 0) $mysqli->query("CALL addToCollection(" . $cid . ", " . $uid . ", " . $pid . ")");
 	echo "<script>window.location = 'collection.php?id=" .  $cid . "';</script>";
 ?>
