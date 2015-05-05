2a87b4be4031a21dd0753a8334550ac67919cbe8
Aaron Dancer <me@aarondancer.com>
2015-05-05 07:47:43 -0500

    Fixed logic issue that prevented adding to collection

--- submit/addToCollection.php
+++ submit/addToCollection.php
@@ -5,6 +5,6 @@
 	$pid = $_POST["pid"];
 	$exists = $mysqli->query("SELECT COUNT(1) AS ex FROM Collection 
 		WHERE CollectionID = " . $cid . " AND ProjectID = ". $pid)->fetch_assoc()["ex"];
-	if ($exists = 0) $mysqli->query("CALL addToCollection(" . $cid . ", " . $uid . ", " . $pid . ")");
+	if ($exists == 0) $mysqli->query("CALL addToCollection(" . $cid . ", " . $uid . ", " . $pid . ")");
 	echo "<script>window.location = 'collection.php?id=" .  $cid . "';</script>";
 ?>
