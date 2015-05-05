41f89c3da0a0a13eaad2085d3ab2807edf49101d
Aaron Dancer <me@aarondancer.com>
2015-05-05 00:13:02 -0500

    Updated with deleteVote function

--- submit/unvote.php
+++ submit/unvote.php
@@ -1,11 +1,8 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script type="text/javascript" src="../js/main.js"></script>
 <?
 	include_once("../connect.php");
 	$ProjectID = $_POST["ProjectID"];
 	$UserID = $_COOKIE["UserID"];
-	$votequery = "DELETE FROM `Vote` WHERE ProjectID = " . $ProjectID . " AND UserID = " . $UserID;
-	$voteresult = $mysqli->query($votequery);
+	$voteresult = $mysqli->query("SELECT deleteVote($ProjectID, $UserID)");
 	if ($voteresult === FALSE) {
 		print_r($mysqli->error_list);
 	}
