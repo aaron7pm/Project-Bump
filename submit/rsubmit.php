038ff4867ac5e9b3e6eb699ba5ac120688844340
Aaron Dancer <me@aarondancer.com>
2015-05-04 23:40:40 -0500

    Simplified createUser

--- submit/rsubmit.php
+++ submit/rsubmit.php
@@ -5,14 +5,10 @@
 	$username = $_POST["inputUsername"];
 	$email = $_POST["inputEmail"];
 	$password = $_POST["inputPassword"];
-	$usercount = $mysqli->query("SELECT COUNT(1) FROM `User`")->fetch_assoc()["COUNT(1)"];
-	while ($mysqli->query("SELECT 1 FROM User WHERE UserID = " . $usercount)->num_rows > 0) {
-		$usercount += 1;
-	}
 	$uexists = $mysqli->query("SELECT userExists(\"$username\") AS uexists")->fetch_assoc()["uexists"];
 	$eexists = $mysqli->query("SELECT userExists(\"$email\") AS eexists")->fetch_assoc()["eexists"];
 	if ($uexists == 0 && $eexists == 0) {
-		$adduser = "SELECT createUser(" . $usercount . ", \"" . $username . "\", \"" . $email . "\", \"" . $password . "\", " . 0 .  ")";
+		$adduser = "SELECT createUser(\"" . $username . "\", \"" . $email . "\", \"" . $password . "\")";
 		$result = $mysqli->query($adduser);
 		if (!$result) {
 			echo "Failed! Please check to see if your entries are valid";
