fa966fad04b44e5d752680008c6423ac91a7ce26
Aaron Dancer <me@aarondancer.com>
2015-05-05 02:36:26 -0500

    Removed unnecessary js

--- submit/npsubmit.php
+++ submit/npsubmit.php
@@ -1,5 +1,3 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script type="text/javascript" src="../js/main.js"></script>
 <?
 	include_once("../connect.php");
 	$name = $_POST["inputName"];
